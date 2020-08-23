5e86cae60cf487f5dcfc60276bdd934b1938fe3c
Michael Barber <60610888+mike-barber@users.noreply.github.com>
2020-08-23 13:11:45 +0100

    nightly feature enable from build.rs

--- fast-linear-estimator/build.rs
+++ fast-linear-estimator/build.rs
@@ -3,7 +3,7 @@
 pub fn main() {
     // Enable "nightly" cfg if the current compiler is nightly.
     if rustc_version::version_meta().unwrap().channel == rustc_version::Channel::Nightly {
-        println!("cargo:rustc-cfg=nightly");
+        println!("cargo:rustc-cfg=feature=\"nightly\"");
         //println!("cargo:rustc-cfg=stdsimd");
     }
 }
