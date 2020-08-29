d5e7d2c7736ab54a221bcaca87d07f3b2b508d2f
Michael Barber <60610888+mike-barber@users.noreply.github.com>
2020-08-29 12:21:19 +0100

    added all targets

--- build-arm.sh
+++ build-arm.sh
@@ -2,4 +2,4 @@
 
 # cross-compile to arm
 # requires nightly; arm intrinsics not stablised yet
-cargo +nightly build --release --target=aarch64-unknown-linux-gnu
+cargo +nightly build --release --target=aarch64-unknown-linux-gnu --all-targets
